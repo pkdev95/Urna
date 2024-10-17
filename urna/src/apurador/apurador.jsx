7d158b344f91e1c01d587d8380befb2183b54a42
Patrick da Costa Gonçalves <113390894+pkdev95@users.noreply.github.com>
2024-10-16 22:36:27 -0300

    Update apurador.jsx

--- urna/src/apurador/apurador.jsx
+++ urna/src/apurador/apurador.jsx
@@ -4,8 +4,7 @@ import "./apurador.css";
 export const Apurador = ({counts}) => {
     return (
             <div className="header">
-                <h3 className="rst">Resultado parcial</h3>
-                <span className="rst">Christyan: {counts.count1}</span> <span className="rst">Patrick: {counts.count3}</span> <span className="rst">Daniel: {counts.count2}</span>
+               <span>Resultado parcial:<span className="rst">Christyan: {counts.count1}</span> <span className="rst">Patrick: {counts.count3}</span> <span className="rst">Daniel: {counts.count2}</span></span>
             </div>
     )
 
